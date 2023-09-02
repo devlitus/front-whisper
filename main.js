a03b092eaeb5d1f3996e94d858d928a79699c9b5
devlitus <developercarles@gmail.com>
2023-09-02 10:45:58 +0200

    add spinner

--- main.js
+++ main.js
@@ -2,21 +2,36 @@ import Dropzone from "dropzone";
 
 let myDropzone = new Dropzone("#form");
 let textArea = document.getElementById("transcript");
+let spinner = document.querySelector(".spinner");
+
+
+myDropzone.on("processing", function (file) { 
+  myDropzone.removeAllFiles();
+  spinner.style.display = "inline";
+});
+
+
+
+
 
 myDropzone.on("addedfile", async (file) => {
   try {
     const formData = new FormData();
     formData.append("file", file);
-    const reponse = await fetch("http://127.0.0.1:5000/whisper", {
+    const response = await fetch("http://127.0.0.1:5000/whisper", {
       method: "POST",
       "Content-Type": "multipart/form-data",
       body: formData,
     });
-    const { results } = await reponse.json();
+    
+    if (response.status !== 200) {
+      throw new Error("Error");
+    }
+    const { results } = await response.json();
     if (results[0].length < 0) {
-      console.log("loading");
       textArea.value += "Loading...";
     }
+    spinner.style.display = "none";
     textArea.value += await results[0].transcript;
     return results[0].transcript;
   } catch (error) {
